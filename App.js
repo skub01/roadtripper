42d221d763d096d6ab22635803983d08011b47cf
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-09-13 16:35:46 -0400

    update button

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to Roadtripper!</Text>
-      <Button title="click here" onPress={() => alert('Button pressed!')}/>
+      <Button title="click here" onPress={() => Alert.alert("Title", "Message",
+      [ {text: "Yes", onPress: () => console.log('Yes')},
+      {text: "No", onPress: () => console.log('No')}
+      ])}/>
       <StatusBar style="auto" />
     </View>
   );
