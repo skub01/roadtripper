fafa2c6675ee7a22c5d6e461f5b5dc98add66230
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-09-13 16:31:53 -0400

    button added to home page

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to Roadtripper!</Text>
+      <Button title="click here" onPress={() => alert('Button pressed!')}/>
       <StatusBar style="auto" />
     </View>
   );
