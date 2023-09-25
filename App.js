61a266e3a07abfa3b69fb2f6708e9894ad1cc8b7
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-09-25 19:24:15 -0400

    install navigation

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
       <Text>Welcome to Roadtripper!</Text>
-      <Button title="click here" onPress={() => Alert.alert("Title", "Message",
+      <Button title="click here" onPress={() => Alert.alert("This is a test", "Testing",
       [ {text: "Yes", onPress: () => console.log('Yes')},
-      {text: "No", onPress: () => console.log('No')},
-      {text: "Maybe", onPress: () => console.log('Maybe')},
+      {text: "No", onPress: () => console.log('No')}
       ])}/>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
