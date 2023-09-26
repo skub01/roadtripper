a5341a393fafc3b013db657dce10af38a1272db4
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-09-25 20:04:32 -0400

    add input to profile screen

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,16 +1,44 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const Profile = () => {
-    return (
-        <View>
-            <Text>This is the profile screen!</Text>
-        </View>
-    )
-}
+  const [name, setName] = useState(''); // State to hold the user's name
+
+  const saveDetails = () => {
+    // Implement your logic to save the user's details (e.g., to a database)
+    // You can use the 'name' state value to save the user's name
+    console.log(`Saving user details: Name - ${name}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>This is the profile screen!</Text>
+
+      <TextInput
+        placeholder="Enter your name"
+        value={name}
+        onChangeText={(text) => setName(text)}
+        style={styles.input}
+      />
+
+      <Button title="Save Details" onPress={saveDetails} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    width: 200,
+    marginBottom: 20,
+  },
+});
+
 export default Profile;
