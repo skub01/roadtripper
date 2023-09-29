17cbdabbf1d974749e27d4f9f89b329989cf3f16
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-09-29 15:51:43 -0400

    add login button to welcome screen

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Button, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "./app/screens/Welcome";
 import Profile from "./app/screens/ProfileScreen";
+import LoginScreen from "./app/screens/LoginScreen";
+import SignUpScreen from "./app/screens/SignupScreen";
 
 const Stack = createNativeStackNavigator();
 
