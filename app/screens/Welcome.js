e17855464f43988631a46921f954dc6a59d54783
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-10-03 13:53:09 -0400

    welcome page styling

--- app/screens/Welcome.js
+++ app/screens/Welcome.js
@@ -4,24 +4,28 @@ import { View, Text, Button, StyleSheet, ImageBackground } from "react-native";
 const Welcome = ({ navigation }) => {
   return (
     <ImageBackground
-    source={require('../assets/bg.jpg')} 
-    style={styles.backgroundImage}
-  >
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to Roadtripper!</Text>
-      <Button
-        title="Log in"
-        onPress={() => {
-          navigation.navigate("Login");
-        }}
-      />
-      <Button
-        title="Sign up"
-        onPress={() => {
-          navigation.navigate("SignUp");
-        }}
-      />
-    </View>
+      source={require('../assets/bg.jpg')}
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Roadtripper</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Log in"
+          onPress={() => {
+            navigation.navigate("Login");
+          }}
+          style={styles.button}
+        />
+        <Button
+          title="Sign up"
+          onPress={() => {
+            navigation.navigate("SignUp");
+          }}
+          style={styles.button}
+        />
+      </View>
     </ImageBackground>
   );
 };
@@ -30,18 +34,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start", 
+    paddingTop: 100,
   },
   welcome: {
     color: 'black',
     fontWeight: 'bold',
-    fontSize: 30,
-    marginTop: -400
+    fontSize: 60,
   },
   backgroundImage: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 150,
+  },
+  button: {
+    color: 'red',
+    fontWeight: 'bold',
+    fontSize: 20, 
   },
 });
 
