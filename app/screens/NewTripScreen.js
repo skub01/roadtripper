2f2cc4af8896c20b4cd4bf159e242153c2651224
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-10-19 15:10:03 -0400

    add inpuit fields to new trip screen

--- app/screens/NewTripScreen.js
+++ app/screens/NewTripScreen.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { Button } from "react-native";
 
-const PastTripsScreen = ({ navigation }) => {
-  return (
-    <>
-    <Text>Let's get roadtripping!</Text>
-</>
-  );
-};
+const NewTripScreen = ({ navigation }) => {
+    const [startLocation, setStartLocation] = useState("");
+    const [endLocation, setEndLocation] = useState("");
+  
+    const saveDetails = () => {
+      console.log(`Saving trip details: Start - ${startLocation}; End - ${endLocation}`);
+    };
+  
+    return (
+      <View style={styles.container}>
+        <Text>Let's get roadtripping!</Text>
+  
+        <TextInput
+          placeholder="Enter your starting location"
+          value={startLocation}
+          onChangeText={(text) => setStartLocation(text)}
+          style={styles.input}
+        />
+  
+        <TextInput
+          placeholder="Enter your destination"
+          value={endLocation}
+          onChangeText={(text) => setEndLocation(text)}
+          style={styles.input}
+        />
+  
+        <Button title="View Trip" onPress={saveDetails} />
+      </View>
+    );
+  };
 
-export default PastTripsScreen;
+export default NewTripScreen;
