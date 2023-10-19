20deab97167bda8b6fde6b83b9aeca2621ff54c5
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-10-18 21:31:10 -0400

    add past trips screen

---
+++ app/screens/PastTripsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-native";
+
+const PastTripsScreen = ({ navigation }) => {
+  return (
+    <>
+    <Text>Past Trips</Text>
+</>
+  );
+};
+
+export default PastTripsScreen;
