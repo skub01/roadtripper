64880daa6fe914926d0c48b84a24351a2f03ff40
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-10-19 15:06:22 -0400

    create new trip screen

---
+++ app/screens/NewTripScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "react-native";
+
+const PastTripsScreen = ({ navigation }) => {
+  return (
+    <>
+    <Text>Let's get roadtripping!</Text>
+</>
+  );
+};
+
+export default PastTripsScreen;
