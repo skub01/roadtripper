07e10c282d692d4d9e459f77d67bf44480c75647
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-10-30 22:44:12 -0400

    create sight card

---
+++ app/screens/SightCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SightCard = () => {
+  return (
+   <text> 
+    This is a sight card.
+    It should contain a title, photo, and maybe time estimate- quick details.
+   </text>
+  );
+};
+
+export default SightCard;
