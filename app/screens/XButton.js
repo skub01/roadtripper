f7e1502a749f00e1c66944c0a7a7c8ed005439a1
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-11-06 20:24:51 -0500

    create XButton component

---
+++ app/screens/XButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "react-native";
+
+const XButton = () => {
+  return (
+    <Button
+      title="X"
+      onPress={() => {
+       
+      }}
+    />
+  );
+};
+
+
+export default XButton;
