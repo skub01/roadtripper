14057f5de5363b793ff85d55278ec4aec416a2e9
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-11-09 09:32:46 -0500

    PastTripsScreen syling

--- app/screens/PastTripsScreen.js
+++ app/screens/PastTripsScreen.js
@@ -2,10 +2,19 @@ import React from "react";
 
 const PastTripsScreen = () => {
   return (
-    <>
+     <View style={styles.container}>
       <Text>Past Trips</Text>
-    </>
+      </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+});
 
 export default PastTripsScreen;
