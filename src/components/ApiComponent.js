2804216ef983e0bae62ac0b2501cc39795f936e3
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-11-22 09:38:24 -0500

    api component styling

--- src/components/ApiComponent.js
+++ src/components/ApiComponent.js
@@ -18,7 +18,7 @@ const ApiComponent = () => {
   }, []); 
 
   return (
-    <View>
+    <View style={styles.container}>
       {data ? (
         <View>
           <Text>Data received from API:</Text>
@@ -30,5 +30,13 @@ const ApiComponent = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
 
 export default ApiComponent;
