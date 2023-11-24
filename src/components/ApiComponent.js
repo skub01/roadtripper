544857ed83bd956404f3033774e09260be45bed2
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-11-24 17:31:57 -0500

    Update ApiComponent imports

--- src/components/ApiComponent.js
+++ src/components/ApiComponent.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const ApiComponent = () => {
   const [data, setData] = useState(null);
