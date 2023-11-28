b3f5b171b8dc8dfbc020d2a5621e252e35de88b3
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-11-28 13:39:07 -0500

    file indexing

--- app/screens/index.js
+++ app/screens/index.js
@@ -1,8 +1,25 @@
 import BackButton from "./BackButton";
-
-
-
+import LoadingScreen from "./LoadingScreen";
+import LoginScreen from "./LoginScreen";
+import NewTripScreen from "./NewTripScreen";
+import PastTripsScreen from "./PastTripsScreen";
+import Profile from "./ProfileScreen";
+import SightCard from "./SightCard"
+import SignupScreen from "./SignupScreen";
+import TripScreen from "./TripScreen";
+import Welcome from "./Welcome";
+import XButton from "./XButton"
 
 export {
     BackButton,
+    LoadingScreen,
+    LoginScreen,
+    NewTripScreen,
+    PastTripsScreen,
+    Profile,
+    SightCard,
+    SignupScreen,
+    TripScreen,
+    Welcome,
+    XButton
 }
