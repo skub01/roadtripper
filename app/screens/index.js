c75c1732df87987731cbfa7b533473e0d94d01f2
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-11-30 14:24:28 -0500

    formatting

--- app/screens/index.js
+++ app/screens/index.js
@@ -4,22 +4,22 @@ import LoginScreen from "./LoginScreen";
 import NewTripScreen from "./NewTripScreen";
 import PastTripsScreen from "./PastTripsScreen";
 import Profile from "./ProfileScreen";
-import SightCard from "./SightCard"
+import SightCard from "./SightCard";
 import SignupScreen from "./SignupScreen";
 import TripScreen from "./TripScreen";
 import Welcome from "./Welcome";
-import XButton from "./XButton"
+import XButton from "./XButton";
 
 export {
-    BackButton,
-    LoadingScreen,
-    LoginScreen,
-    NewTripScreen,
-    PastTripsScreen,
-    Profile,
-    SightCard,
-    SignupScreen,
-    TripScreen,
-    Welcome,
-    XButton
-}
+  BackButton,
+  LoadingScreen,
+  LoginScreen,
+  NewTripScreen,
+  PastTripsScreen,
+  Profile,
+  SightCard,
+  SignupScreen,
+  TripScreen,
+  Welcome,
+  XButton,
+};
